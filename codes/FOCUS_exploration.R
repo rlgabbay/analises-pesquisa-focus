3b7bb742be572368eef2c11db11093aad578817c
Rafael Gabbay <rafael.lopes.gabbay@gmail.com>
2022-06-16 17:11:04 -0300

    Create FOCUS_exploration.R

---
+++ codes/FOCUS_exploration.R
@@ -0,0 +1,10 @@
+##############################################################################
+# Author : Rafael Gabbay
+# Date: 20.05.2022
+# Email: rafael.lopes.gabbay@gmail.com
+# Goal: Exploring rbcb package 
+
+########################### Libraries ######################################
+
+pacman::p_load(rbcb,
+               tidyverse)
